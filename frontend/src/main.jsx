508dd56e6cdec15c69fe5d372a7ac8e85cde6ec6
Thong Nguyen <thongnt.it@gmail.com>
2026-04-24 15:40:13 +0700

    feat(frontend): install @clerk/clerk-react, wrap app in ClerkProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.jsx'
 import ReloadPrompt from './components/ReloadPrompt'
@@ -7,14 +8,19 @@ import AppLoader from './components/AppLoader'
 import DatabaseErrorBoundary from './components/DatabaseErrorBoundary'
 import OfflineIndicator from './components/OfflineIndicator'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DatabaseErrorBoundary>
-      <ReloadPrompt />
-      <AppLoader>
-        <App />
-      </AppLoader>
-      <OfflineIndicator />
-    </DatabaseErrorBoundary>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <DatabaseErrorBoundary>
+        <ReloadPrompt />
+        <AppLoader>
+          <App />
+        </AppLoader>
+        <OfflineIndicator />
+      </DatabaseErrorBoundary>
+    </ClerkProvider>
   </StrictMode>
 )
